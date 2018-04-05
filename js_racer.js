7c08892ddaddc407116ff0b656e7f94e4499b143
Natascha Lamsu <tazcha15@gmail.com>
2018-04-06 00:39:13 +0700

    js racer

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,12 @@
 "use strict"
 
-function dice(){
+var argv = process.argv
+var player = Number(argv[2])
+var track = Number(argv[3])
 
+function dice(){
+ var random = Math.floor(Math.random() * (7 - 1) + 1)
+ return random
 }
 
 function sleep(milliseconds) {
@@ -13,24 +18,79 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
-
+var players = []
+function generatePlayer(player) {
+  for(var i = 0; i < player; i++) {
+    var playersInfo = {}
+    playersInfo.name = String.fromCharCode(i+97)
+    playersInfo.pos = 0
+    players.push(playersInfo)
+  }
 }
 
-function print_line(player, pos) {
+generatePlayer(player)
 
+function print_board(track, pos, players) {
+ var trackBoard = []
+ for(var i = 0; i < track; i++) {
+   if(i === pos) {
+     trackBoard.push(players)
+   } else {
+     trackBoard.push(' ')
+   }
+ }
+ return trackBoard.join('|')
 }
 
-function advanced_player(player) {
-
+function print_line() {
+  for(var i = 0; i < players.length; i++) {
+    console.log(print_board(track, players[i].pos, players[i].name))
+  }
 }
 
-function finished() {
+print_line()
 
-}
-function winner() {
+function advanced_player() {
+  if(player < 2) {
+    console.log('player minimal 2');
+  }
+  if(track < 15) {
+    console.log('panjang track minimal 15');
+  }
 
+  var name = ''
+  var win = false
+  while(win === false) {
+    for(var i = 0; i < players.length; i++) {
+      if(!win) {
+        players[i].pos += dice()
+      }
+      if(finished(players[i].pos)) {
+        win = true
+        players[i].pos = track-1
+        name += players[i].name
+      }
+      console.log(print_board(track, players[i].pos, players[i].name))
+    }
+    sleep(3000);
+    reset_board();
+  }
+  console.log(winner(name));
 }
+
+function finished(pos) {
+  if(pos >= track-1) {
+    // pos = track-1
+    return true
+  }
+}
+
+function winner(name) {
+  return 'The winner is ' + name
+}
+
 function reset_board() {
   console.log("\x1B[2J")
 }
+
+advanced_player()
