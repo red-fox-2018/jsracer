88e396dce3a98e7dcf5f425a99b70e2c15e1e2c8
asepridwan1989 <aseprmail@gmail.com>
2018-04-05 11:11:50 +0700

    done

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,7 @@
 "use strict"
-
+const argv = process.argv
 function dice(){
-
+  return Math.ceil(Math.random() * 6 + 1)
 }
 
 function sleep(milliseconds) {
@@ -13,24 +13,86 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
-
+function print_board(player) {
+  var board = '';
+  for (var i = 0; i < player.length; i++) {
+    board += print_line(player[i], pos[i]) + '\n';
+  }
+  return board;
 }
 
 function print_line(player, pos) {
-
+  var inLine = [];
+  for (var i = 0; i < track; i++) {
+    inLine.push(' |');
+  }
+  inLine[pos] = player + '|';
+  var disp = inLine.join('');
+  return disp;
 }
 
-function advanced_player(player) {
-
+function advanced_player(playPos) {
+  playPos = playPos + dice() - 1;
+  if (playPos >= track - 1) {
+    playPos = track - 1;
+    finished(pos, track);
+  }
+  return playPos;
 }
 
-function finished() {
-
+function playerGen(player) {
+	var playerName = 'abcdefghijklmnopqrstuvwxyz';
+	var playerPlay = '';
+	for (var i = 0; i < player; i++) {
+		playerPlay += playerName[i];
+	}
+	return playerPlay;
 }
-function winner() {
 
+function finished(pos,track) {
+  for (var i = 0; i < pos.length; i++) {
+    if (pos[i] >= track - 1) {
+      return true;
+    }
+  }
+  return false;
+}
+function winner(player) {
+    return `Player  ${player}  is the winner`
 }
 function reset_board() {
   console.log("\x1B[2J")
+}
+
+sleep(1000);
+reset_board();
+var track = argv[3];
+var jmlPlayer = argv[2];
+if (jmlPlayer < 2 || jmlPlayer === undefined) {
+	console.log('pemain minimal = 2');
+} else if (track < 15 || track === undefined) {
+	console.log('track minimum = 15');
+} else {
+	var pos = [];
+	var stoper = 0;
+	for (var i = 0; i < jmlPlayer; i++) {
+		pos.push(0);
+	}
+	var playerName = playerGen(jmlPlayer);
+	console.log(print_board(playerName, track));
+	sleep(1000);
+	reset_board();
+	while (finished(pos, track) === false) {
+		for (var i = 0; i < pos.length; i++) {
+			pos[i] = advanced_player(pos[i]);
+			console.log(print_board(playerName));
+			sleep(1000);
+			reset_board();
+			if (finished(pos, track) === true) {
+				console.log(print_board(playerName));
+				console.log(winner(playerName[i]));
+				break;
+			}
+		}
+	}
 }
