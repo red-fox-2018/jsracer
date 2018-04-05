fc4849c96c98fa63fb868418e9f6862cf0896d1a
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-04-05 17:04:34 +0700

    anchor telah selesai, namun kode belum dry

--- js_racer.js
+++ js_racer.js
@@ -1,7 +1,5 @@
-"use strict"
-
-function dice(){
-
+function dice(range){//mengenerate angka random sampai batas yang dimasukkan pada parameter
+  return Math.floor(Math.random()*range)
 }
 
 function sleep(milliseconds) {
@@ -13,24 +11,86 @@ function sleep(milliseconds) {
   }
 }
 
-function print_board() {
-
-}
-
-function print_line(player, pos) {
-
-}
-
-function advanced_player(player) {
-
-}
-
-function finished() {
-
-}
-function winner() {
-
+function participants(totalPlayer) { // outputnya adalah list pemain dalam sebuah array, dan posisinya
+  let huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let hasil = []
+  for (var i = 0; i < totalPlayer; i++) {
+    hasil.push([])
+    hasil[i].push(0)
+    hasil[i].push(huruf[dice(huruf.length)])
+  }
+  debugger
+  return hasil //[[A,0],[B,0]]
 }
 function reset_board() {
   console.log("\x1B[2J")
 }
+
+function startRacing(totalPlayer,trackDist) {
+  var condition = true
+  var participantsInitial = participants(totalPlayer)//[[A,0],[B,0]]
+  let partInit = participantsInitial.slice()
+  function print_board(totalPlayer,trackDist) {
+    if (condition === true) {
+      for (var row = 0; row < totalPlayer; row++) { //print per row
+        let track = []
+        for (var pos = 0; pos < trackDist; pos++) {// print per character
+          if (pos == 0) {
+            track.push(participantsInitial[row][1])
+            track.push('|')
+          } else {
+            track.push(' ')
+            track.push('|')
+          }
+        }
+        console.log(track.join(''));
+        debugger
+      }
+      condition = false
+    } else {
+      for (var row = 0; row < totalPlayer; row++) { //print per row
+        let track = []
+        let newPos = dice(Math.round(6))
+        while (newPos === 0) {
+          newPos = dice(Math.round(6))
+        }
+        participantsInitial[row][0] += newPos
+        let batas = false
+        if (participantsInitial[row][0] >= trackDist) {
+          participantsInitial[row][0] = trackDist-1
+          batas = true
+        }
+        for (var pos = 0; pos < trackDist; pos++) {// print per character
+          if (pos == participantsInitial[row][0]) {
+            if (batas === true) {
+              track.push(' ')
+              track.push('|')
+              track.push(participantsInitial[row][1])
+            } else {
+              track.push(participantsInitial[row][1])
+              track.push('|')
+            }
+          } else {
+            track.push(' ')
+            track.push('|')
+          }
+        }
+        console.log(track.join(''));
+      }
+    }
+    return
+  }
+  while(partInit[partInit.length-1][0] < trackDist-1) {
+    partInit.sort()
+    print_board(totalPlayer,trackDist);
+    if (partInit[partInit.length-1][0] < trackDist - 1) {
+      sleep(1000)
+      reset_board()
+    }
+  }
+  console.log(`Player ${partInit[partInit.length-1][1]} is the winner`);
+  return
+}
+
+let argv = process.argv
+startRacing(argv[2],argv[3])
